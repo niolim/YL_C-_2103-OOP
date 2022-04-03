07fbf2316d46c4500006b5775cf1fe59b48ba23d
YL <e.lugovskoy94@gmail.com>
2022-04-03 22:08:58 +0600

    Lesson 3 (Done)

---
+++ Lesson 3/Lesson_3_Part_1.1/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Введите номер вашего банковского счета");
+int myAcc = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine("Введите номер счета на который хотите осуществить перевод денежных средств");
+int someAcc = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine($"Какую сумму вы хотите перевести на счет {someAcc} ? ");
+int moneyToSend = int.Parse(Console.ReadLine());
+
+BankAccount process = new BankAccount(myAcc, moneyToSend);
+
+process.Print();
+
+
+class BankAccount
+{
+    private int accId;
+    private int moneyToSend;
+    
+
+    public BankAccount(int a1, int m1) { accId = a1; moneyToSend = m1; }
+       
+
+    public void Print ()
+    {
+        Console.WriteLine($"Со счета {accId} было переведено {moneyToSend}$");
+    }
+   
+}
+
---
+++ Lesson 3/Lesson_3_Part_1/Program.cs
@@ -0,0 +1,77 @@
+﻿BankAccount newAccount = new BankAccount();
+
+Console.WriteLine("Введите номер вашего банковского счета");
+int myAcc = int.Parse(Console.ReadLine());
+newAccount.AccId = myAcc;
+
+Console.WriteLine("Какую сумму вы хотите положить на свой счет?");
+int myMoney = int.Parse(Console.ReadLine());
+newAccount.AccBalance = myMoney;
+Console.Clear();
+newAccount.PrintMyInfo();
+Console.WriteLine("Нажмите любую клавишу для продолжения операций");
+Console.ReadKey();
+
+Console.WriteLine();
+
+Console.WriteLine("Введите номер счета на который хотите осуществить перевод денежных средств");
+int someAcc = int.Parse(Console.ReadLine());
+newAccount.AccId2 = someAcc;
+
+
+Console.WriteLine($"Какую сумму вы хотите перевести на счет {someAcc} ? ");
+int moneyToSend = int.Parse(Console.ReadLine());
+newAccount.AccBalance2 = moneyToSend;
+Console.Clear();
+newAccount.PrintLastInfo();
+Console.ReadKey();
+
+
+
+class BankAccount
+{
+    private int accId;
+    private int accId2;
+    private int accBalance;
+    private int accBalance2;
+
+    public int AccId
+    {
+        get { return accId; }
+        set { accId = value; }
+    }
+
+    public int AccId2
+    {
+        get { return accId2; }  
+        set { accId2 = value; }
+    }
+
+    public int AccBalance
+    {
+        get { return accBalance; }
+        set { accBalance = value; }
+    }
+
+    public int AccBalance2
+    {
+        get { return accBalance2; }
+        set { accBalance2 = value; }
+    }
+
+     public void PrintMyInfo()
+    {
+        Console.WriteLine(
+            $"Номер вашего счета - {AccId} {Environment.NewLine}" +
+            $"Сумма на вашем счету - {AccBalance}");
+    }
+
+    public void PrintLastInfo()
+    {
+        Console.WriteLine(
+            $"Со счета {AccId} был совершен перевод на сумму {AccBalance2}$ на счет {AccId2}.{Environment.NewLine}" +
+            $"Остаток средств на вашем счету {AccBalance - AccBalance2}.");
+    }
+
+}
+
