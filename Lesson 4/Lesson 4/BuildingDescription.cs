abfde26bca510b79a44e10104f2a6776ddc44c4c
YL <e.lugovskoy94@gmail.com>
2022-04-08 10:51:32 +0600

    Lesson 4 (Done)

---
+++ Lesson 4/Lesson 4/BuildingDescription.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_4
+{
+    internal class BuildingDescription
+    {
+
+
+
+
+        public Guid iD;
+
+        private string name;
+        private int floors; // от 1 до 12
+        private int appartments; // от 1 до 5
+        private int entrances; // от 1 до 10
+
+        public void SetName (string n)
+        {
+            name = n;
+        }
+
+        public void SetFloors (int f)
+        {
+            if (f <= 0)
+            {
+                Console.WriteLine("Не верно указано кол-во этажей. Будет установленно минимальное значение - 1");
+                floors = 1;
+                return;
+            }
+
+            if (f > 12)
+            {
+                Console.WriteLine("Не верно указано кол-во этажей. Будет установленно максимальное значение - 12");
+                floors = 12;
+                return;
+            }
+
+                      
+                floors = f; 
+                      
+        }
+
+        public void SetApp (int a)
+        {
+            if (a <= 0)
+            {
+                Console.WriteLine("Не верно указано кол-во квартир на этаже. Будет установленно минимальное значение - 1");
+                appartments = 1;
+                return;
+            }
+
+            if (a > 5)
+            {
+                Console.WriteLine("Не верно указано кол-во квартир на этаже. Будет установленно максимальное значение - 5");
+                appartments = 5;
+                return;
+            }
+
+
+            appartments = a;
+
+        }
+
+        public void SetEnt (int e)
+        {
+            if (e <= 0)
+            {
+                Console.WriteLine("Не верно указано кол-во подъездов. Будет установленно минимальное значение - 1");
+                entrances = 1;
+                return;
+            }
+
+            if (e > 10)
+            {
+                Console.WriteLine("Не верно указано кол-во подъездов. Будет установленно максимальное значение - 10");
+                entrances = 10;
+                return;
+            }
+
+
+            entrances = e;
+
+        }
+
+      
+        public void PrintInfo ()
+        {
+            Console.WriteLine(
+                $"Наименование дома - {name} {Environment.NewLine}" +
+                $"Регистрационный номер дома - {iD} {Environment.NewLine}" +
+                $"Количество этажей в доме - {floors} {Environment.NewLine}" +
+                $"Количество подъездов в доме - {entrances} {Environment.NewLine}" +
+                $"Количество квартир на одном этаже - {appartments} {Environment.NewLine}");
+        }
+
+
+
+    }
+}
---
+++ Lesson 4/Lesson 4/Program.cs
@@ -0,0 +1,55 @@
+﻿using Lesson_4;
+
+
+BuildingDescription building1 =  new BuildingDescription();
+building1.iD = Guid.NewGuid();
+Console.WriteLine("Дайте имя новому жилому комплексу");
+building1.SetName(Console.ReadLine());
+Console.WriteLine("Выберите кол-во этажей в одном доме от 1 до 12");
+building1.SetFloors(int.Parse(Console.ReadLine()));
+Console.WriteLine("Выберите кол-во подъездов в одном доме от 1 до 10");
+building1.SetEnt(int.Parse(Console.ReadLine()));
+Console.WriteLine("Выберите кол-во квартир на одном этаже от 1 до 5");
+building1.SetApp(int.Parse(Console.ReadLine()));
+
+Console.Clear();
+
+BuildingDescription building2 = new BuildingDescription();
+building2.iD = Guid.NewGuid();
+Console.WriteLine("Дайте имя новому жилому комплексу");
+building2.SetName(Console.ReadLine());
+Console.WriteLine("Выберите кол-во этажей в одном доме от 1 до 12");
+building2.SetFloors(int.Parse(Console.ReadLine()));
+Console.WriteLine("Выберите кол-во подъездов в одном доме от 1 до 10");
+building2.SetEnt(int.Parse(Console.ReadLine()));
+Console.WriteLine("Выберите кол-во квартир на одном этаже от 1 до 5");
+building2.SetApp(int.Parse(Console.ReadLine()));
+
+Console.Clear();
+
+BuildingDescription building3 = new BuildingDescription();
+building3.iD = Guid.NewGuid();
+Console.WriteLine("Дайте имя новому жилому комплексу");
+building3.SetName(Console.ReadLine());
+Console.WriteLine("Выберите кол-во этажей в одном доме от 1 до 12");
+building3.SetFloors(int.Parse(Console.ReadLine()));
+Console.WriteLine("Выберите кол-во подъездов в одном доме от 1 до 10");
+building3.SetEnt(int.Parse(Console.ReadLine()));
+Console.WriteLine("Выберите кол-во квартир на одном этаже от 1 до 5");
+building3.SetApp(int.Parse(Console.ReadLine()));
+
+Console.Clear();
+
+building1.PrintInfo();
+
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+
+building2.PrintInfo();
+
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+
+building3.PrintInfo();
