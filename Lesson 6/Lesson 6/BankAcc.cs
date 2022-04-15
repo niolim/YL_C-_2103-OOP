0a3b272a7a514bfa8abbcccd9b2543ad050a91e2
YL <e.lugovskoy94@gmail.com>
2022-04-15 10:02:32 +0600

    Lesson 6 (Done)

---
+++ Lesson 6/Lesson 6/BankAcc.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_6
+{
+    internal class BankAcc
+    {
+        private int bankAcc;
+        private string bankName;
+
+        public BankAcc(int x, string y)
+        {
+            bankAcc = x;
+            bankName = y;
+        }
+
+        public override bool Equals (object obj)
+        {
+            if (obj == null || GetType() != obj.GetType()) 
+                return false;
+            BankAcc bank = (BankAcc)obj;
+            return (bankAcc == bank.bankAcc);
+        }
+
+        public override int GetHashCode()
+        {
+            return bankAcc ;  
+        }
+
+        public override string ToString()
+        {
+
+            return $"{bankName} -- {bankAcc}";
+
+        }
+
+    }
+}
