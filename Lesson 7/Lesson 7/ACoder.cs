630ea13f969989673a0ba3659a853c633251d35f
YL <e.lugovskoy94@gmail.com>
2022-04-18 16:27:52 +0600

    Lesson 7(Done)

---
+++ Lesson 7/Lesson 7/ACoder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_7
+{
+    internal class ACoder : ICoder
+    {
+        public string Encode(string enc)
+        {
+            string encodeText = "";
+            for (int i = 0; i < enc.Length; i++)
+            {
+                int n = enc[i] + 1;
+                encodeText = encodeText + (char)n;
+            }
+            return encodeText;
+        }
+
+        public string Decode(string dec)
+        {
+            string decodeText = "";
+            for (int i = 0; i < dec.Length; i++)
+            {
+                int n = dec[i] - 1;
+                decodeText = decodeText + (char)n;
+            }
+            return decodeText;
+        }
+
+
+
+
+    }
+}
---
+++ Lesson 7/Lesson 7/BCoder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_7
+{
+    internal class BCoder : ICoder
+    {
+        readonly int key;
+        public BCoder(int k)
+        {
+            key = k;
+        }
+
+        public string Encode(string enc)
+        {
+            string encodeText = ""; 
+            for (int i = 0; i < enc.Length; i++)
+            {
+                int n = enc[i] ^ key;
+                encodeText = encodeText + (char)n;
+            }
+
+            return encodeText;
+        }
+
+        public string Decode(string dec)
+        {
+            string decodeText = ""; 
+            for (int i = 0; i < dec.Length; i++)
+                decodeText += (char)(dec[i] ^ key);
+            return decodeText;
+        }
+
+        
+    }
+}
